eba042807fdb266a0795c4318bba9e0d6795aec9
Denis Sancov <denis.sancov@gmail.com>
2021-03-14 22:54:45 +0200

    https to git

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/denis-sancov/FCB-utils.git", .upToNextMajor(from: "1.0.1")),
-        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.6.0"))
+        .package(url: "https://github.com/tadija/AEXML.git", .upToNextMajor(from: "4.6.0")),
+        .package(url: "git@github.com:denis-sancov/FCB-utils.git", .upToNextMajor(from: "1.0.1"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
